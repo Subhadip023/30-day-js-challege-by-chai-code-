6e8291a7a6d086b0a6331c3f20669b2acfec8cb7
Subhadip023 <subhadip240420@gmail.com>
2024-07-17 15:49:00 +0530

    day 4 kjh

--- day_4.js
+++ day_4.js
@@ -6,38 +6,38 @@
 
 // Task 1: Write a program to print numbers from 1 to 10 using a for loop.
 
-// for (let index = 1; index <= 10; index++) {
-// //   console.log(index);
-// }
+for (let index = 1; index <= 10; index++) {
+  console.log(index);
+}
 
 // Task 2: Write a program to print the multiplication table of 5 using a for loop.
 
-// for (let index = 1; index <= 10; index++) {
-//     // console.log(5+" X "+index +" = " +5*index);
-//   }
+for (let index = 1; index <= 10; index++) {
+    console.log(5+" X "+index +" = " +5*index);
+  }
 
 // Activity 2: While Loop
 // Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
 
-// let sum=0;
-// let i=1;
-// while (i<=10) {
-//     sum+=i;
-//     i++;
-// }
-// console.log("The Sum of numbers from 1 to 10 = ",sum)
+let sum=0;
+let i=1;
+while (i<=10) {
+    sum+=i;
+    i++;
+}
+console.log("The Sum of numbers from 1 to 10 = ",sum)
 
 // Task 4: Write a program to print numbers from 10 to 1 using a while loop.
 
-// i=10;
+i=10;
 
-// while (i>=1) {
-//     // console.log(i--)
-// }
+while (i>=1) {
+    console.log(i--)
+}
 
 // Activity 3: Do...While Loop
 // Task 5: Write a program to print numbers from 1 to 5 using a do...while loop.
- let i=1;
+  i=1;
 do {
     
     console.log(i++)
@@ -60,10 +60,12 @@ console.log("Factorial of "+realNumber+" is "+factorial)
 // Activity 4: Nested Loops
 // Task 7: Write a program to print a pattern using nested for loops:
 
-for (let i = 0; i <5; i++) {
-    for (let j = 0; j < 5; j++) {
-        print('*')
+for (let i = 0; i <=5; i++) {
+    let str=''
+    for (let j = 0; j < i; j++) {
+        str+='*'
     }
+    console.log(str)
 }
 
 // *
